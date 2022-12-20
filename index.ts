b533457b0d2318c40b70e8206c8b67b38c289da3
atomisadeveloper <atomdevelops@protonmail.com>
2022-12-20 15:12:34 +0000

    feat: added support for cli

--- index.ts
+++ index.ts
@@ -1,30 +1,51 @@
 import * as fs from "fs";
+import * as path from "path";
 import { marked } from "marked";
+import yargs from "yargs";
 
-const inputDir = "./input";
-const outputDir = "./output";
+// Define the compile command
+yargs.command({
+  command: "compile <input> <output>",
+  describe:
+    "Compile Markdown files in the input directory to HTML files in the output directory",
+  builder: (yargs: any) => {
+    return yargs
+      .positional("input", {
+        describe: "The input directory",
+        type: "string",
+      })
+      .positional("output", {
+        describe: "The output directory",
+        type: "string",
+      });
+  },
+  handler: (argv: any) => {
+    const inputDir = argv.input;
+    const outputDir = argv.output;
 
-// Check if the input directory exists
-if (fs.existsSync(inputDir)) {
-  // Read the contents of the input directory
-  const files = fs.readdirSync(inputDir);
+    // Read the contents of the input directory
+    fs.readdir(inputDir, (err, files) => {
+      if (err) {
+        console.error(`Error reading input directory: ${err}`);
+        return;
+      }
 
-  // Filter the list of files to only include markdown files
-  const markdownFiles = files.filter((file) => file.endsWith(".md"));
+      // Iterate over the files in the input directory
+      for (const file of files) {
+        // Check if the file is a Markdown file
+        if (path.extname(file) === ".md") {
+          // Read the contents of the Markdown file
+          fs.readFile(path.join(inputDir, file), "utf8", (readErr, data) => {
+            if (readErr) {
+              console.error(`Error reading file ${file}: ${readErr}`);
+              return;
+            }
 
-  // If there are markdown files in the input directory
-  if (markdownFiles.length > 0) {
-    // Iterate over the markdown files
-    markdownFiles.forEach((file) => {
-      // Read the contents of the markdown file
-      const markdown = fs.readFileSync(`${inputDir}/${file}`, "utf8");
+            // Convert the Markdown content to HTML
+            const html = marked(data);
 
-      // Convert the markdown to HTML
-      const html = marked(markdown);
-
-      // Add a stylesheet to the HTML
-      const styledHtml = `
-      <html>
+            const styledHtml = `
+               <html>
         <head>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
           <style>
@@ -35,13 +56,20 @@ if (fs.existsSync(inputDir)) {
               display: grid;
               grid-template-columns: 250px 1fr;
             }
+            a {
+              text-decoration: none;
+              color: black;
+            }
+            a:hover {
+              text-decoration: underline;
+              text-underline-offset: 2px;
+            }
             .toc {
               grid-column: 1;
             }
             .content {
               grid-column: 2;
             }
-
             .toc h2 {
               margin-left: 15px;
             }
@@ -65,7 +93,6 @@ if (fs.existsSync(inputDir)) {
           <script>
             // Extract headings from the content
             const headings = document.querySelectorAll('main h1, main h2, main h3, main h4');
-
             // Generate the TOC
             const toc = document.createElement('ul');
             headings.forEach(heading => {
@@ -76,22 +103,46 @@ if (fs.existsSync(inputDir)) {
               li.appendChild(a);
               toc.appendChild(li);
             });
-
             // Insert the TOC into the page
             document.querySelector('.toc').appendChild(toc);
           </script>
         </body>
       </html>
-      `;
+            `;
 
-      // Check if the output directory exists
-      if (!fs.existsSync(outputDir)) {
-        // Create the output directory if it doesn't exist
-        fs.mkdirSync(outputDir);
+            // Write the HTML content to a new file in the output directory
+            const outputPath = path.join(
+              outputDir,
+              path.basename(file, ".md") + ".html"
+            );
+            fs.writeFile(outputPath, styledHtml, (writeErr) => {
+              if (writeErr) {
+                console.error(`Error writing file ${outputPath}: ${writeErr}`);
+                return;
+              }
+
+              console.log(`Successfully converted ${file} to ${outputPath}`);
+            });
+          });
+        }
       }
-
-      // Write the styled HTML to a file in the output directory
-      fs.writeFileSync(`${outputDir}/${file}.html`, styledHtml);
     });
-  }
-}
+  },
+});
+
+// Parse the command-line arguments
+yargs.parse();
+
+yargs.command({
+  command: "help",
+  describe: "Show the help text",
+  handler: () => {
+    yargs.showHelp();
+  },
+});
+
+// Show the help text if no command is specified
+yargs.showHelpOnFail(true);
+
+// Parse the command-line arguments
+yargs.parse();
---
+++ input/lemonade.md
@@ -0,0 +1,3 @@
+# Lemonade
+
+some lemonade
